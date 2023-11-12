333501bbcef0840dd970d3241547c8c8bd45bf57
Artsiom Marozau <>
2023-11-12 20:50:22 +0100

    feat: api get product list

--- shop-be/product-service/src/functions/getProductsList/handler.ts
+++ shop-be/product-service/src/functions/getProductsList/handler.ts
@@ -1,9 +1,40 @@
-import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
-import { formatJSONResponse } from '@libs/api-gateway';
-import Mock from '@mock/mock-products.json'
+import type { ValidatedEventAPIGatewayProxyEvent } from "@libs/api-gateway";
+import { formatJSONResponse } from "@libs/api-gateway";
+import getDatabase from "@model/database";
+
+const getStockItems = async () => {
+  try {
+    console.log('Fetching stock items')
+    const { Stock, Product } = await getDatabase()
+    const stockItems = await Stock.findAll({
+      include: [{
+        model: Product,
+      }],
+    });
+    console.log('Fetched stock items', stockItems)
+
+    console.log('Start mapping stock items')
+    const result = stockItems.map(item => ({
+      productId: item.product_id,
+      productTitle: item.product?.title,
+      count: item.count,
+    }));
+    console.log('End mapping stock items', result)
+    return result;
+  } catch (error) {
+    console.error('Error fetching stock items:', error);
+    throw error;
+  }
+};
 
 export const main: ValidatedEventAPIGatewayProxyEvent<void> = async () => {
-  return formatJSONResponse(
-    [...Mock.body]
-  );
+  try {
+    return formatJSONResponse(await getStockItems());
+  } catch (error) {
+    console.log("Error while getting the stock list", error);
+    return formatJSONResponse(
+      { message: `Internal server error` },
+      { statusCode: 500 }
+    );
+  }
 };
--- shop-be/product-service/src/functions/index.ts
+++ shop-be/product-service/src/functions/index.ts
@@ -1,3 +1,2 @@
 export { default as getProductsList } from "./getProductsList";
-export { default as getAvailableProductsList } from "./getAvailableProductsList";
 export { default as getProductsById } from "./getProductsById";
